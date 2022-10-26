57f70b595e811036c83d590b8db4c51d48a984ce
beniwbogdan <bohdanbeniv@gmail.com>
2022-10-26 19:38:15 +0200

    00_components base

---
+++ src/00-components/Rating.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Rating() {
+    return (
+        <div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+
+        </div>
+    );
+}
+
+export default Rating;
